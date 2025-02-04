e3df3dac85604d26067328288dd2a1ecd4613768
melque <melque_ex@yahoo.com.br>
2025-02-04 16:24:31 -0300

    update

--- setup.py
+++ setup.py
@@ -9,19 +9,19 @@ classifiers = [
 ]
  
 setup(
-  name='pipefyUts',
-  version='1.1.0',
-  description='Pipefy manipulator',
-  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  long_description_content_type='text/markdown',
-  url='',  
-  author='Melque Lima',
-  author_email='melque_ex@yahoo.com.br',
-  license='MIT', 
-  classifiers=classifiers,
-  keywords='pipefyUts', 
-  packages=find_packages(),
-  package_data={'': ['*.gql']},
-  include_package_data=True,
-  install_requires=["requests"]
+  name                          = 'pipefyUts',
+  version                       = '1.1.1',
+  description                   = 'Pipefy manipulator',
+  long_description              = open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description_content_type = 'text/markdown',
+  url                           = '',
+  author                        = 'Melque Lima',
+  author_email                  = 'melque_ex@yahoo.com.br',
+  license                       = 'MIT',
+  classifiers                   = classifiers,
+  keywords                      = 'pipefyUts',
+  packages                      = find_packages(),
+  package_data                  = {'': ['*.gql']},
+  include_package_data          = True,
+  install_requires              = ["requests"]
 )
